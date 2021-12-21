fe0cccf4dfd4fbc43b93b2e28a3e8e23120b3cfa
Alexandre Oyama <94652462+alexandre-999@users.noreply.github.com>
2021-12-21 18:33:15 +0900

    課題手直し完了3

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,11 @@
-'use strict'
-       while(true) {
-        const result = prompt('日本の首都は？')
-        if(result === '東京') {
-          break;
-        }else{
-          alert('不正解です！')
-        }
-      }
-      alert('正解です！') 
+'use strict' ;
+
+while( true ){
+ const result = prompt( '日本の首都は？' ) ;
+ if( result === '東京' ) {
+   alert( '正解です！' ) ;
+   break ;
+ } else {
+   alert( '不正解です！' ) ;
+ } ;
+} ;
