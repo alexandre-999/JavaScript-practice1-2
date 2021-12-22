6501cbc473242a135d5d1bd36caca2110b1de418
Alexandre Oyama <94652462+alexandre-999@users.noreply.github.com>
2021-12-22 18:10:18 +0900

    ）後の空白の訂正完了

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,10 @@
 'use strict' ;
 
 while( true ) {
- const result = prompt( '日本の首都は？' ) ;
+ const result = prompt( '日本の首都は？' );
  if( result === '東京' ) {
-   alert( '正解です！' ) ;
+   alert( '正解です！' );
    break ;
  } else {
-   alert( '不正解です！' ) ;
+   alert( '不正解です！' );
  }} ;
